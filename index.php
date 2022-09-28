d0332e532a57c566e73beed5f64bc3ac5d3593ff
JaladeAlan <74614392+JaladeAlan@users.noreply.github.com>
2022-09-28 21:19:53 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+ 
+    <?php include 'add/head.php';  ?>
+      
+  <body>
+    <?php include 'add/navbar.php';  ?>
+
+    <!-- END header -->
+
+    <section class="site-hero overlay" data-stellar-background-ratio="0.5" style="background-image: url(images/course_2.jpg);">
+      <div class="container">
+        <div class="row align-items-center justify-content-center site-hero-inner">
+          <div class="col-md-10">
+  
+            <div class="mb-5 element-animate">
+              <div class="block-17">
+                <h2 class="heading text-center mb-4">Quick Access To Courses</h2>
+               
+                <p class="text-center mb-5">Knowledge At Your Fingertips</p>
+                <p class="text-center"><a href="courses.php" class="btn py-3 px-5">Get Started</a></p>
+              </div>
+            </div>
+        
+          </div>
+        </div>
+      </div>
+    </section>
+   <!-- END section -->
+      <br>
+      
+        <div class="row justify-content-center mb-5 element-animate">
+          <div class="col-md-7 text-center section-heading">
+            <h4 class="text-primary heading">Vision</h4>
+            <p>The Vision of the Computer Science Department, Tai Solarin College of Education is to produce knowledgeable, skillfull, dynamic, innovative and value-driven teachers, evaluators and personnel that are relevant to the society and  can match their peers nationally and internationally</p>
+          </div>
+        </div>
+
+        <div class="row justify-content-center mb-5 element-animate">
+          <div class="col-md-7 text-center section-heading">
+            <h4 class="text-primary heading">Mission of the Department</h4>
+            <p>Providing academically sound and professionally relevant programmes and training for prospective teachers who are current in scientific knowledge, training and well grounded in modern techniques that advance the education industry</p>
+          </div>
+        </div>
+
+        <div class="row justify-content-center mb-5 element-animate">
+          <div class="col-md-7 text-center section-heading">
+            <h4 class="text-primary heading">Philosophy of the Department</h4>
+            <p>The Philosophy of the programme is derived from the desire to train sound, committed and dynamic computer science educators for primary, secondary and post-secondary schools, whoc can effectively respond to the present challenges of education in their immediate community, the nation and the world, as well as responding to the computer phenomena that become part of all spheres of life</p>
+          </div>
+        </div>
+  
+        
+
+<?php include 'add/footer.php'; ?>
+    <!-- END footer -->
+    
+    <!-- loader -->
+    <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#f4b214"/></svg></div>
+  
+<?php include 'add/scripts.php' ?>
+  </body>
+</html>
